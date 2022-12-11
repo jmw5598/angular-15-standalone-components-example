db922ddb91b4adc4675df3df6283f5b1be085a4a
Jason White <jmw5598@gmail.com>
2022-12-11 08:40:12 -0500

    initial setup of standalone components application from default application the cli scaffolds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'foo-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    NgSwitch, 
+    NgSwitchCase, 
+    NgSwitchDefault
+  ]
 })
 export class AppComponent {
   title = 'angular-15-standalone-components-example';
