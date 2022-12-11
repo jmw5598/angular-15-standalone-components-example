df80982908da92ac3dfc1db2876066a19b0542fa
Jason White <jmw5598@gmail.com>
2022-12-11 08:34:22 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'foo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-15-standalone-components-example';
+}
